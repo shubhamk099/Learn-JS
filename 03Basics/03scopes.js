cc9810bf1f5056a2e0227dd9a95a5a95fdf98f15
shubhamk099 <shubhamkkumar9797@gmail.com>
2024-07-06 20:24:49 +0530

    Scopes in JS

---
+++ 03Basics/03scopes.js
@@ -0,0 +1,14 @@
+
+if(true){
+    let first = 100;
+    const second = 200;
+    var third = 300;
+};
+
+//console.log(first);
+//console.log(second);
+console.log(third);             // This shouldn't be accessible outside of if block scope
+
+
+// -> Variables declared with var keyword have global scope.
+// -> Variables declared with let & const keyword have local scope.
